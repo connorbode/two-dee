0e7977d1ec24cda104226c3582eae6f2bbd44f75
Connor Bode <connor.bode@gmail.com>
2015-04-18 20:28:50 -0400

    add a concat method

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-var gulp  = require('gulp');
-var mocha = require('gulp-mocha');
+var gulp    = require('gulp');
+var mocha   = require('gulp-mocha');
+var concat  = require('gulp-concat');
 
 gulp.task('test', function () {
   return gulp.src(['spec/config.js', 'src/**/*.spec.js'], { read: 'false' })
     .pipe(mocha({ reporter: 'min' }));
+});
+
+gulp.task('concat', function () {
+  return gulp.src(['src/**/*.js', '!src/**/*.spec.js'])
+    .pipe(concat('two-dee.js'))
+    .pipe(gulp.dest('dist'));
 });
