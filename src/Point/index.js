437b1069187faed1d0ab061f18b0db4dd712b63b
Connor Bode <connor.bode@gmail.com>
2015-04-18 20:22:37 -0400

    created a simple point

---
+++ src/Point/index.js
@@ -0,0 +1,12 @@
+// Point
+// -----
+// 
+// A point is simply a point on the 2D canvas.
+// 
+// The parameters, `x` and `y` are the point's 
+// x and y coordinates.
+
+module.exports = function (x, y) {
+  this.x = x;
+  this.y = y;
+};
---
+++ src/Point/index.spec.js
@@ -0,0 +1,15 @@
+describe('Point', function () {
+
+  var Point;
+
+  before(function (done) {
+    Point = require('./index');
+    done();
+  });
+
+  it('is accessible', function () {
+    var point = new Point(1, 2);
+    point.x.should.equal(1);
+    point.y.should.equal(2);
+  });
+});
