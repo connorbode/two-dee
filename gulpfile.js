366d7e011571f96b6f15b122b5d543d9416e0449
Connor Bode <connor.bode@gmail.com>
2015-04-18 20:30:35 -0400

    add a doc method

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp');
 var mocha   = require('gulp-mocha');
 var concat  = require('gulp-concat');
+var docco   = require('gulp-docco');
 
 gulp.task('test', function () {
   return gulp.src(['spec/config.js', 'src/**/*.spec.js'], { read: 'false' })
@@ -11,4 +12,11 @@ gulp.task('concat', function () {
   return gulp.src(['src/**/*.js', '!src/**/*.spec.js'])
     .pipe(concat('two-dee.js'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('doc', function () {
+  var options = {};
+  return gulp.src(['dist/two-dee.js'])
+    .pipe(docco(options))
+    .pipe(gulp.dest('doc'));
 });
