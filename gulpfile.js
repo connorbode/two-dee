46b642c58a263b17897eea8b78bef3cc0dd93671
Connor Bode <connor.bode@gmail.com>
2015-04-18 20:35:22 -0400

    add a compression task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp    = require('gulp');
 var mocha   = require('gulp-mocha');
 var concat  = require('gulp-concat');
 var docco   = require('gulp-docco');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
 
 gulp.task('test', function () {
   return gulp.src(['spec/config.js', 'src/**/*.spec.js'], { read: 'false' })
@@ -19,4 +21,13 @@ gulp.task('doc', function () {
   return gulp.src(['dist/two-dee.js'])
     .pipe(docco(options))
     .pipe(gulp.dest('doc'));
+});
+
+gulp.task('uglify', function () {
+  return gulp.src(['dist/two-dee.js'])
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest('dist'));
 });
