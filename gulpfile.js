13966b73dfff7dd1ccb9255a922bdd161988ad35
Connor Bode <connor.bode@gmail.com>
2015-04-21 21:54:37 -0400

    properly build & bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp    = require('gulp');
-var mocha   = require('gulp-mocha');
-var concat  = require('gulp-concat');
-var docco   = require('gulp-docco');
-var uglify  = require('gulp-uglify');
-var rename  = require('gulp-rename');
+var gulp        = require('gulp');
+var mocha       = require('gulp-mocha');
+var concat      = require('gulp-concat');
+var docco       = require('gulp-docco');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+var browserify  = require('browserify');
+var globby      = require('globby');
+var through     = require('through2');
+var buffer      = require('vinyl-buffer');
+var source      = require('vinyl-source-stream');
 
 gulp.task('test', function () {
   return gulp.src(['spec/config.js', 'src/**/*.spec.js'], { read: 'false' })
@@ -32,4 +37,26 @@ gulp.task('uglify', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['concat', 'doc', 'uglify']);
+gulp.task('bundle', function () {
+  var bundleStream = through();
+
+  bundleStream
+    .pipe(source('./main.js'))
+    .pipe(buffer())
+    .pipe(rename(function (path) {
+      path.basename = 'two-dee';
+    }))
+    .pipe(gulp.dest('./dist'));
+
+  globby(['./main.js'], function (err, files) {
+    var b = browserify({
+      entries: files
+    });
+
+    b.bundle().pipe(bundleStream);
+  });
+
+  return bundleStream;
+});
+
+gulp.task('build', ['bundle', 'doc', 'uglify']);
---
+++ main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'Circle': require('./src/Circle'),
+  'Line':   require('./src/Line'),
+  'Point':  require('./src/Point')
+};
